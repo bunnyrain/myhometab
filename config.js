bb5ba5323915fe456ea1a05b50550b4f75a6878f
bunnyrain <165784129+bunnyrain@users.noreply.github.com>
2025-08-21 09:01:14 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -11,22 +11,23 @@ const CONFIG = {
 	// └─┘┴ ┴└─┘┴└─┘└─┘
 
 	// General
-	name: 'John',
+	name: 'Reaper.',
 	imageBackground: false,
 	openInNewTab: true,
 	twelveHourFormat: false,
 
 	// Greetings
 	greetingMorning: 'Good morning!',
-	greetingAfternoon: 'Good afternoon,',
-	greetingEvening: 'Good evening,',
-	greetingNight: 'Go to Sleep!',
+	greetingAfternoon: 'What are you up to this time?,',
+	greetingEvening: 'Good evening youngin,',
+	greetingNight: 'Go to sleep lil bro!',
 
 	// Layout
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: 'InsertYourAPIKeyHere123456', // Write here your API Key
+	weatherKey: 'c6ba9c0f798bfc5e97a547abf20c979e
+', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
